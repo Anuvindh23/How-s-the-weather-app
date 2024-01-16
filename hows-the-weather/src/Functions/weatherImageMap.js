3020f9765b937da8fa548c00724d657e5907e7d9
Anuvindh <yoursanuvindh@gmail.com>
2024-01-16 22:54:37 +0530

    jpg's converted to avif

--- hows-the-weather/src/Functions/weatherImageMap.js
+++ hows-the-weather/src/Functions/weatherImageMap.js
@@ -1,13 +1,13 @@
-import mistImage from "../assets/mist_smoke_haze_fog.jpg";
-import smokeImage from "../assets/mist_smoke_haze_fog.jpg";
-import hazeImage from "../assets/mist_smoke_haze_fog.jpg";
-import fogImage from "../assets/mist_smoke_haze_fog.jpg";
-import clearImage from "../assets/clear.jpg";
-import cloudsImage from "../assets/clouds.jpg";
-import DrizzleImage from "../assets/drizzle.jpg";
-import ThunderstormImage from "../assets/thunderstorm.jpg";
-import TornadoImage from "../assets/tornado.jpg";
-import SnowImage from "../assets/snow.jpg";
+import mistImage from "../assets/mist_smoke_haze_fog.avif";
+import smokeImage from "../assets/mist_smoke_haze_fog.avif";
+import hazeImage from "../assets/mist_smoke_haze_fog.avif";
+import fogImage from "../assets/mist_smoke_haze_fog.avif";
+import clearImage from "../assets/clear.avif";
+import cloudsImage from "../assets/clouds.avif";
+import DrizzleImage from "../assets/drizzle.avif";
+import ThunderstormImage from "../assets/thunderstorm.avif";
+import TornadoImage from "../assets/tornado.avif";
+import SnowImage from "../assets/snow.avif";
 
 const weatherStockImages = {
     Mist: mistImage,
